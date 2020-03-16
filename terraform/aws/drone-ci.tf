46a8b0801f8ef593f89ec9f0ed2f43246fc58cee
Matheus Fidelis <matheus@nanoshots.com.br>
2020-03-15 21:14:35 -0300

    drone ci

---
+++ terraform/aws/drone-ci.tf
@@ -0,0 +1,67 @@
+resource "aws_instance" "droneci" {
+
+    ami = var.ami
+    instance_type = var.droneci_instance_type
+
+    subnet_id = aws_subnet.public_subnet.id
+    associate_public_ip_address = true
+
+    vpc_security_group_ids = [ 
+        aws_security_group.droneci.id
+    ]
+
+    key_name = aws_key_pair.cluster_key.key_name
+
+    tags = merge(var.tags, { 
+        Name = format("%s-droneci", var.project_name)
+    })
+
+}
+
+resource "aws_security_group" "droneci" {
+
+    name        = format("%s-droneci-sg", var.project_name)
+
+    vpc_id      = aws_vpc.cluster_vpc.id
+
+    ingress {
+        from_port   = "22"
+        to_port     = "22"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = "80"
+        to_port     = "80"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = "443"
+        to_port     = "443"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = "5000"
+        to_port     = "5000"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = merge(var.tags, { 
+        Name = format("%s-droneci-sg", var.project_name)
+    })
+
+
+}
